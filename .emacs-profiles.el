a7ea91125eadef6d33e86455784b3b69decf830e
leo3 <texas.cyberthal@gmail.com>
2021-03-19 07:14:12 +0800

    add profile for Spacemacs syl main

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,15 +1,14 @@
 (
  ("default" . ((user-emacs-directory . "~/.emacs.d/")
-               (env . (("SPACEMACSDIR" . "~/.spacemacs.d/")))
-               )
-  )
+               (env . (("SPACEMACSDIR" . "~/.spacemacs.d/")))))
+
  ("dev" . ((user-emacs-directory . "~/.chemacs/Spacemacs/Dev/.emacs.d/")
            (env . (("SPACEMACSDIR" . "~/.chemacs/Spacemacs/Dev/.spacemacs.d/")))
-           (server-name . "Spacemacs-Dev")
-           )
-  )
- ("vanilla" . ((user-emacs-directory . "~/.chemacs/Vanilla/Dev/.emacs.d/")
-               (server-name . "Vanilla-Dev")
-               )
-  )
+           (server-name . "Spacemacs-Dev")))
+
+ ("gnu" . ((user-emacs-directory . "~/.chemacs/Gnu/Main/.emacs.d/")
+           (server-name . "Gnu-Main")))
+
+ ("spac-syl-main" . ((user-emacs-directory . "~/.chemacs/Spacemacs/Syl20bnr/Main/.emacs.d/")
+                     (server-name . "Spac-syl-main")))
  )
